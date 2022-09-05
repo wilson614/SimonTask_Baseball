f5292f938f2e5392de1c6905b37a115f8fc3e04f
wilson614 <s110613053@stu.ntue.edu.tw>
2022-09-06 03:54:05 +0800

    feat: use left hand to open the rule page during game

---
+++ Assets/Scripts/HandController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandController : MonoBehaviour
+{
+    [SerializeField] GameObject hand;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hand.active)
+        {
+            RuleManager.Instance.RuleTurnOn();
+        } else
+        {
+            RuleManager.Instance.RuleTurnOff();
+        }
+    }
+}
